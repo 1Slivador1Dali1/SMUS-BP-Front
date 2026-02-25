43896dbcebe5935e6371ea6f29635eb1ded365b0
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2026-02-26 01:42:01 +0300

    feat: DashboardPage, Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { House, LayoutListIcon, SettingsIcon } from "lucide-vue-next";
+</script>
+<template>
+  <nav class="nav">
+    <ul class="nav-list">
+      <li class="nav-list-item"><House class="nav-list-item__icon" /></li>
+      <li class="nav-list-item">
+        <LayoutListIcon class="nav-list-item__icon" />
+      </li>
+      <!-- <li class="nav-list-item"><House class="nav-list-item__icon" /></li> -->
+      <li class="nav-list-item">
+        <SettingsIcon class="nav-list-item__icon" />
+      </li>
+    </ul>
+  </nav>
+</template>
+<style scoped>
+.nav {
+  position: fixed;
+  bottom: 40px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+  background-color: #1f1f1f;
+  color: #fff;
+  border-radius: 20px;
+}
+
+.nav-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.nav-list-item {
+  padding: 8px 8px 2px;
+  border-radius: 12px;
+  color: #42b883;
+  transition: all 0.3s ease-in-out;
+}
+
+.nav-list-item:hover {
+  background-color: #2f2f2f;
+}
+
+.nav-list-item__icon {
+  width: 30px;
+  height: 30px;
+  margin: 0;
+}
+</style>
---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">Dali</div>
+</template>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 1200px;
+  width: 100%;
+  min-height: 400px;
+  padding: 40px;
+  background-color: #2f2f2f;
+  border: 2px solid #42b883;
+  color: #fff;
+  border-radius: 40px;
+}
+</style>
