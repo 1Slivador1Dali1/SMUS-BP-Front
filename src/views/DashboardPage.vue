467ffd2812e817d4c92a65ea5ee26688df8deac3
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2026-02-26 03:09:18 +0300

    refactor: styles

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -4,19 +4,4 @@
   <div class="container">Dali</div>
 </template>
 
-<style scoped>
-.container {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 1200px;
-  width: 100%;
-  min-height: 400px;
-  padding: 40px;
-  background-color: #2f2f2f;
-  border: 2px solid #42b883;
-  color: #fff;
-  border-radius: 40px;
-}
-</style>
+<style scoped></style>
